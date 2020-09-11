57a9252fc95f3ea1e2a158e00695d047e34fd251
Vladyslav Holubtsov <vladysik1912@gmail.com>
2020-09-12 02:57:55 +0300

    Commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введіть номер закінчення послідовності n");
+            Int64 n= Int64.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(Fibonachi(i)+" ");
+            }
+            
+        }
+        static Int64 Fibonachi(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            else if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fibonachi(n - 1) + Fibonachi(n - 2);
+            }
+        }
+    }
+}
